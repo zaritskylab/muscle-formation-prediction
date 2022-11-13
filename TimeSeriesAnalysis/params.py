21bdd4dc4c857591295a31c1577c93998160ed19
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2022-11-13 11:23:43 +0200

    re-packaged data-preprocessing code

--- TimeSeriesAnalysis/params.py
+++ TimeSeriesAnalysis/params.py
@@ -1,8 +1,8 @@
 # intensity
-window_size = 16
-tracks_len = 30
-diff_window = [140, 170]
-con_window = [[0, 30], [40, 70], [90, 120], [140, 170], [180, 210], [220, 250]]
+window_size = 16  # window crop on actin intensity measurements
+tracks_len = 30  # number of frames to train the model on
+diff_window = [140, 170]  # frames ro train the model on
+con_window = [[0, 30], [40, 70], [90, 120], [140, 170], [180, 210], [220, 250]] # frames ro train the model on
 local_density = False
 
 wt_cols = [wt for wt in range(0, 260, tracks_len)]
